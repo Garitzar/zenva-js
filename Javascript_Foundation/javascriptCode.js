e41d8c205fd89050dcfa68b7d590df75f4330dd9
Jonas Sandroos <jonas_sandroos16@hotmail.se>
2020-08-20 20:42:59 +0200

    HTML events

--- Javascript_Foundation/javascriptCode.js
+++ Javascript_Foundation/javascriptCode.js
@@ -1,2 +1,11 @@
-var i = 5;
-console.log(i);
+var input = document.getElementById("input");
+var button = document.getElementById("button");
+var output = document.getElementById("output");
+
+function displayOutput() {
+    const text = input.value;
+    output.innerHTML = text;
+}
+
+//button.addEventListener("click", displayOutput);
+button.onclick = displayOutput;
