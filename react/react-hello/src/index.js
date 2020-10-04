eb12cb7b148d6b302ad2fb3e78cdf858a88e50c6
Garitzar <jonas_sandroos16@hotmail.se>
2020-10-04 13:16:42 +0200

    Props examples

--- react/react-hello/src/index.js
+++ react/react-hello/src/index.js
@@ -1,20 +1,72 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+/// HelloWorld Start
+// function HelloWorld(){
+//   return (
+//     <div>
+//       <Hello/>!
+//     </div>
+//   );
+// }
 
-function HelloWorld(){
-  return (
-    <div>
-      <Hello/>!
-    </div>
-  );
+// function Hello(){
+//   const name = "World!";
+//   return(
+//   <span>Hello {name}</span>
+//   );
+// }
+
+/// HelloWorld End
+
+/// Jonas Start
+// function Jonas(){
+//     const fName = "Jonas";
+//     const lName = "Sandroos";
+
+//     return(
+//         <Person age = {27}
+//                 name = {fName + " " + lName} />
+//     )
+// }
+
+// const Person = props => <h1>{props.name + " " + props.age}</h1>
+// ReactDOM.render(<Jonas/>, document.querySelector('#root'));
+/// Jonas End
+
+/// HelloProp Start
+// function HelloProp(props){
+//     return(
+//         <span>Hello {props.name}</span>
+//     )
+// }
+
+// ReactDOM.render(<HelloProp name="Jonas"/>, document.querySelector('#root'));
+/// HelloProp End
+
+/// HelloParameter Start
+// const HelloParameter = ({name}) => (
+//     <span>Hello {name}</span>
+// )
+
+// ReactDOM.render(<HelloParameter name="Jonas"/>, document.querySelector('#root'));
+/// HelloParameter End
+
+/// HelloParameter2 Start
+// const HelloParameter2 = ({fName, lName}) => (
+//     <span>Hello {fName} {lName}</span>
+// )
+
+// ReactDOM.render(<HelloParameter2 fName="Jonas" lName="Sandroos"/>, document.querySelector('#root'));
+/// HelloParameter End
+
+/// HelloParameter3 Start
+const HelloParameter3 = (props) => {
+    const {name} = props;
+    return(
+    <span>Hello, {name}!</span>
+    )
 }
 
-function Hello(){
-  const name = "World!";
-  return(
-  <span>Hello {name}</span>
-  );
-}
-
-ReactDOM.render(<HelloWorld/>, document.querySelector('#root'));
+ReactDOM.render(<HelloParameter3 name="Jonas Sandroos"/>, document.querySelector('#root'));
+/// HelloParameter3 End
