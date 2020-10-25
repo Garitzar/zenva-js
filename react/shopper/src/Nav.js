060e1f1b9a188600a3e23e7ba973340cf18b22a3
Garitzar <jonas_sandroos16@hotmail.se>
2020-10-25 18:07:12 +0100

    Start of Shopper project

---
+++ react/shopper/src/Nav.js
@@ -0,0 +1,18 @@
+import React from "react";
+ 
+const Nav = () => {
+  return (
+    <nav className="App-nav">
+      <ul>
+        <li className="App-nav-item">
+          <a href="/#">Items</a>
+        </li>
+        <li className="App-nav-item">
+          <a href="/#">Cart</a>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+ 
+export default Nav;
