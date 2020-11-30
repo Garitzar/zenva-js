4d0499dc29e92154923cf69993bc022162cfdb19
Garitzar <jonas_sandroos16@hotmail.se>
2020-11-30 17:32:39 +0100

    Added state.

--- react/shopper/src/App.js
+++ react/shopper/src/App.js
@@ -1,16 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import Nav from "./Nav";
 import "./App.css";
  
+function Tab(name, content){
+    this.name = name;
+    this.content = content;
+};
+
+const Items = new Tab('items', <span>the items</span>);
+const Cart = new Tab('cart', <span>the cart</span>);
+
+const Tabs = [Items, Cart];
+
 const App = () => {
-  return (
-    <div className="App">
-      <Nav />
-      <main className="app-content">
-        <span>Empty</span>
-      </main>
-    </div>
-  );
+    const [activeTab, setActiveTab] = useState(Tabs[0]);
+    return (
+        <div className="App">
+        <Nav
+            tabs = {Tabs}
+            activeTab = {activeTab}
+            onTabChange = {setActiveTab}
+        />
+        <main className="app-content">
+            <span>
+                <Content tab={activeTab} />
+            </span>
+        </main>
+        </div>
+    );
+};
+
+const Content = ({tab}) => {
+    return tab.content;
 };
  
 export default App;
--- react/shopper/src/Nav.js
+++ react/shopper/src/Nav.js
@@ -1,18 +1,25 @@
 import React from "react";
  
-const Nav = () => {
-  return (
+const Nav = ({tabs, activeTab, onTabChange}) => {
+    const itemClass = (tabName) =>
+        `App-nav-item ${ activeTab.name === tabName ? "selected" : ""}`;
+        
+    return (
     <nav className="App-nav">
-      <ul>
-        <li className="App-nav-item">
-          <a href="/#">Items</a>
-        </li>
-        <li className="App-nav-item">
-          <a href="/#">Cart</a>
-        </li>
-      </ul>
+        <ul>
+            <li className={itemClass(tabs[0].name)}>
+                <a href="/#" onClick={() => onTabChange(tabs[0])}>
+                    Items
+                </a>
+            </li>
+            <li className={itemClass(tabs[1].name)}>
+                <a href="/#" onClick={() => onTabChange(tabs[1])}>
+                    Cart
+                </a>
+            </li>
+        </ul>
     </nav>
-  );
+    );
 };
  
 export default Nav;
